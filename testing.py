faf42695e25cd2a95015502b252c7b2b6d39bc90
ronitafter <54575125+ronitafter@users.noreply.github.com>
2023-01-05 21:00:32 +0200

    new

---
+++ testing.py
@@ -0,0 +1,32 @@
+# print("ggggg")
+# age = 30
+# age = 31
+# print("age:", age)
+
+# num = 5.6
+# f_n = "r"
+# is_true = False
+# print("num:", num, ",f_n:", f_n, ",is_true:", is_true)
+
+# name = input("what is your name? ")
+# print("hello " + name)
+
+# year = input("year: ")
+# age = 2022 - int(year)
+# print(age)
+
+num1 = float(input("num1: "))
+num2 = float(input("num2: "))
+sum = num1 + num2
+print(sum)
+
+
+# For the weight converter i use 1kg = 2.2046 lbs
+# weight = float(input("Weight: "))
+# unit = str(input("(K)g or (L)bs: "))
+# if unit == "k" or unit == "K":
+#     lbs = weight * 2.2046
+#     print("Weight in Lbs: " + str(round(lbs, 2)))
+# elif unit == "l" or unit == "L":
+#     kg = weight / 2.2046
+#     print("Weight in Kg: " + str(round(kg, 2)))
