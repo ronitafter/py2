a06ab622442044228672525690328b9ab38c0543
ronitafter <54575125+ronitafter@users.noreply.github.com>
2023-01-10 00:10:00 +0200

    New

--- testing.py
+++ testing.py
@@ -54,11 +54,70 @@
 # print(x)
 
 
-price = 1000
+# price = 1000
 # print(price > 10 and price < 200)
 # print(price < 10 or price < 200)
-print(not price > 10)
+# print(not price > 10)
 
 # and
-# or
-# not
+# # or
+# # not
+
+# # price = 5
+# # print(price > 10 and price < 30)  # true true = true
+# # print(price > 10 or price < 30)  # true false = true
+# # print(not price > 10)  # true false = true
+
+
+# temp = -7
+
+# if temp >= 30:
+#     print("its noy winter")
+# elif temp >= 20:
+#     print("its almost winter")
+# elif temp >= 10:
+#     print("its winter")
+# else:
+#     print("its very winter")
+# print("done")
+
+# weight = int(input("weight : "))
+# unit = input("(K)g or (L)bs: ")
+# if unit.upper() == "K":
+#     converted = weight / 0.45
+#     print("weight in lbs: " + str(converted))
+# else:
+#     converted = weight * 0.45
+#     print("weight in Kg: " + str(converted))
+
+# i = 1
+
+# while i <= 10:
+#     print(i * '*')
+#     i = i + 1
+
+# names = ['a', 'v', 'u', 'f', 's', 'x']
+# names[0] = 'A'
+# print(names[0:3])
+# print(names)
+# print(names[0])
+# print(names[-1])
+# print(names[-2])
+
+# nums = [1, 2, 3, 4, 5]
+# nums.append(6)
+# nums.insert(0, 0)
+# nums.remove(3)
+# nums.clear()
+# print(nums)
+# print(1 in nums)
+# print(len(nums))
+
+# nums = [1, 2, 3, 4, 5]
+# for item in nums:
+#     print(item)
+
+nums = range(5, 10, 2)
+# print(nums)
+for number in nums:
+    print(number)
